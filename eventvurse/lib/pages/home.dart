716fe18348844d9450084f770b1ad619e7c4e72e
gracieux-dev <chacoungracieux@gmail.com>
2023-09-21 14:01:29 +0200

    modified:   eventvurse/lib/main.dart 	new file:   eventvurse/lib/pages/home.dart

---
+++ eventvurse/lib/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatelessWidget {
+  const HomePages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
